75fb6a5fb6f62fbb3292043f2f8945f5dc4bd146
dm-carmo <davidcarmo.1997@gmail.com>
2025-05-16 23:57:12 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 // Call updateContent() on page load
-window.addEventListener('DOMContentLoaded', async () => {
-    const userPreferredLanguage = localStorage.getItem('lang') || 'en';
-    const langData = await fetchLanguageData(userPreferredLanguage);
-    updateContent(langData);
+window.addEventListener("DOMContentLoaded", async () => {
+  const userPreferredLanguage = localStorage.getItem("lang") || "en";
+  const langData = await fetchLanguageData(userPreferredLanguage);
+  updateContent(langData);
 
-    const langBtn = document.querySelector("#langBtn");
-    langBtn.setAttribute("class", userPreferredLanguage);
+  const langBtn = document.querySelector("#langBtn");
+  langBtn.setAttribute("class", userPreferredLanguage);
 });
 
 // Function to update content based on selected language
 function updateContent(langData) {
-    document.querySelectorAll('[data-i18n]').forEach(element => {
-        const key = element.getAttribute('data-i18n');
-        element.innerHTML = langData[key];
-    });
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const key = element.getAttribute("data-i18n");
+    element.innerHTML = langData[key];
+  });
 }
 
 // Function to set the language preference
 function setLanguagePreference(lang) {
-    localStorage.setItem('lang', lang);
-    location.reload();
+  localStorage.setItem("lang", lang);
+  location.reload();
 }
 
 // Function to fetch language data
 async function fetchLanguageData(lang) {
-    const response = await fetch(`lang/${lang}.json`);
-    return response.json();
+  const response = await fetch(`lang/${lang}.json`);
+  return response.json();
 }
 
 // Function to change language
 async function changeLanguage(lang) {
-    await setLanguagePreference(lang);
-    
-    const langData = await fetchLanguageData(lang);
-    updateContent(langData);
-    
-    const langBtn = document.querySelector("#langBtn");
-    langBtn.setAttribute("class", lang);
+  await setLanguagePreference(lang);
+
+  const langData = await fetchLanguageData(lang);
+  updateContent(langData);
+
+  const langBtn = document.querySelector("#langBtn");
+  langBtn.setAttribute("class", lang);
 }
